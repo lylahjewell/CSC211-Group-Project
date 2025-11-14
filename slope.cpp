adbb0a3136a8afe78ec70281a1c71ebd7d9e0ba8
romanstriker-ux <lytvynrom@gmail.com>
2025-11-13 19:48:42 -0500

    Add files via upload

---
+++ slope.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+void CalcSlope(double a, double b){
+    double slope = 0;
+    if(b != 0){
+        slope = -a /b;
+        cout << slope << endl;
+    }
+    else{
+        cout << "Line is vertical" << endl;;
+    }
+}
+
+int main(){
+
+    double a = 10;
+    double b = 0;
+    CalcSlope(a, b);
+    return 0;
+}
